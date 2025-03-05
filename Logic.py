3b658db7a5b4d1c69b44028dcb18d6f359048fb1
Juan Sebastian Pinilla Giraldo <pinillagiraldoj@gmail.com>
2025-03-05 00:13:46 -0500

    def payment_fee_calc_while_studying and def payment_fee_calc_after_studying
    only names, no logic in it

---
+++ Logic.py
@@ -0,0 +1,9 @@
+credit_type = input("enter the type of credit you want")
+college_enrollment = input("enter the ammount of college enrollment per semester")
+semesters = input("enter the number of semesters you want to calculate for")
+
+def payment_fee_calc_while_studying(credit_type, college_enrollment, semesters):
+    pass
+
+def payment_fee_calc_after_studying(credit_type, college_enrollment, semesters):
+    pass
