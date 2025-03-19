9651fbe7fe3a83af7fa18977f03f0e33e3fc3518
Samuel Gil <samuelgilest@gmail.com>
2025-03-19 09:08:20 -0500

    Updated exceptions
    El mensaje de las excepciones ya está definido en las clases de excepciones (exceptions.py), el código ahora está un poco más limpio

--- Logic.py
+++ Logic.py
@@ -41,41 +41,41 @@ def ask_information():
 def payment_fee_calc_while_studying(credit_type, college_enrollment, semesters):
     if credit_type == 1:
         if college_enrollment <= 0:
-            raise CollegeEnrollmentError("Invalid college enrollment")
+            raise CollegeEnrollmentError()
         if semesters <= 0 or isinstance(semesters, float):
-            raise SemestersError("Invalid number of semesters")
+            raise SemestersError()
         payment_fee =  (0.3 * college_enrollment * semesters* medium_term_credit_interest_rate_30) / (1 - (1 + medium_term_credit_interest_rate_30)**(-1 * (semesters/2) * 12))
     elif credit_type == 2:
         if college_enrollment <= 0:
-            raise CollegeEnrollmentError("Invalid college enrollment")
+            raise CollegeEnrollmentError()
         if semesters <= 0 or isinstance(semesters, float):
-            raise SemestersError("Invalid number of semesters")
+            raise SemestersError()
         payment_fee =  (0.6 * college_enrollment * semesters * medium_term_credit_interest_rate_60) / (1 - (1 + medium_term_credit_interest_rate_60)**(-1 * (semesters/2) * 12))
     elif credit_type == 3:
         if college_enrollment <= 0:
-            raise CollegeEnrollmentError("Invalid college enrollment")
+            raise CollegeEnrollmentError()
         if semesters <= 0 or isinstance(semesters, float):
-            raise SemestersError("Invalid number of semesters")
+            raise SemestersError()
         payment_fee = (college_enrollment * semesters * short_term_credit_interest_rate_100) / (1 - (1 + short_term_credit_interest_rate_100)**(-1 * (semesters/2) * 12))
     else:
-        raise CreditTypeError("Invalid credit type")
+        raise CreditTypeError()
     return payment_fee
 
 def payment_fee_calc_after_studying(credit_type, college_enrollment, semesters):
     if credit_type == 1:
         if college_enrollment <= 0:
-            raise CollegeEnrollmentError("Invalid college enrollment")
+            raise CollegeEnrollmentError()
         if semesters <= 0 or isinstance(semesters, float):
-            raise SemestersError("Invalid number of semesters")
+            raise SemestersError()
         payment_fee =  (0.7 * college_enrollment * semesters* medium_term_credit_interest_rate_30) / (1 - (1 + medium_term_credit_interest_rate_30)**(-1.5 * (semesters/2) * 12))
     elif credit_type == 2:
         if college_enrollment <= 0:
-            raise CollegeEnrollmentError("Invalid college enrollment")
+            raise CollegeEnrollmentError()
         if semesters <= 0 or isinstance(semesters, float):
-            raise SemestersError("Invalid number of semesters")
+            raise SemestersError()
         payment_fee =  (0.4 * college_enrollment * semesters * medium_term_credit_interest_rate_60) / (1 - (1 + medium_term_credit_interest_rate_60)**(-1.5 * (semesters/2) * 12))
     elif credit_type == 3:
         return None
     else:
-        raise CreditTypeError("Invalid credit type")
+        raise CreditTypeError()
     return payment_fee
