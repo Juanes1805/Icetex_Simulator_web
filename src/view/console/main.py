eb914229e3f1b2bd50a097ef75375c3881b892ab
Samuel GG <samuelgilest@gmail.com>
2025-03-21 02:25:04 -0500

    code cleaned with pylint

--- src/view/console/main.py
+++ src/view/console/main.py
@@ -1,17 +1,26 @@
+"""This module contains the main function of the console application."""
 import sys
+from model.logic import (
+    payment_fee_calc_while_studying, ask_information, payment_fee_calc_after_studying
+)
 sys.path.append("src")
-from model.logic import payment_fee_calc_while_studying, ask_information, payment_fee_calc_after_studying
+
 def main():
+    """Main function of the console application."""
     print("---- Welcome to the ICETEX simulator :p ----")
     while True:
         credit_type, college_enrollment, semesters = ask_information()
-        fee_while_studying = payment_fee_calc_while_studying(credit_type, college_enrollment, semesters)
+        fee_while_studying = payment_fee_calc_while_studying(
+            credit_type, college_enrollment, semesters
+        )
         print(f"Your monthly fee while studying is: {fee_while_studying}")
-        fee_after_studying = payment_fee_calc_after_studying(credit_type, college_enrollment, semesters)
+        fee_after_studying = payment_fee_calc_after_studying(
+            credit_type, college_enrollment, semesters
+        )
         print(f"Your monthly fee after studying is: {fee_after_studying}")
 
         if input("Do you want to calculate another fee? (yes/no): ").lower() != "yes":
-            print("Thanks for using the ICETEX simulator. Goodbye!")   
+            print("Thanks for using the ICETEX simulator. Goodbye!")
             break
 
 if __name__ == '__main__':
