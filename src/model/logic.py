dc34ed1348f80feefe5b04dc65e63cbac92b09ae
Samuel GG <samuelgilest@gmail.com>
2025-03-21 02:12:14 -0500

    code cleaned with pylint

--- src/model/logic.py
+++ src/model/logic.py
@@ -1,18 +1,19 @@
-import unittest
+"""This module contains the logic of the application."""
 import sys
+from model.exception import CreditTypeError, CollegeEnrollmentError, SemestersError
 sys.path.append("src")
 
-from model.exception import CreditTypeError, CollegeEnrollmentError, SemestersError
 
-
-medium_term_credit_interest_rate_30 = 0.0115 #1,15% month expired
-medium_term_credit_interest_rate_60 = 0.099 #0,99% month expired
-short_term_credit_interest_rate_100 = 0.099 #0,99% month expired
+MEDIUM_TERM_CREDIT_INTEREST_RATE_30 = 0.0115 #1,15% month expired
+MEDIUM_TERM_CREDIT_INTEREST_RATE_60 = 0.099 #0,99% month expired
+SHORT_TERM_CREDIT_INTEREST_RATE_100 = 0.099 #0,99% month expired
 
 
 def ask_information():
+    """Ask the user for the information needed to calculate the payment fee."""
     while True:
-        credit_type = input("Enter the type of credit you want \n 1 for 30%\n 2 for 60%\n 3 for 100%\n")
+        credit_type = input("Enter the type of credit you want \n 1 for 30%\n 2 for 60%\n"
+        "3 for 100%\n")
         list_options = ["1", "2", "3"]
         if credit_type in list_options:
             credit_type = int(credit_type)  # We convert to integer after validating the input
@@ -21,7 +22,8 @@ def ask_information():
 
     while True:
         try:
-            college_enrollment = float(input("Enter the amount of college enrollment per semester: "))
+            college_enrollment = float(input("Enter the amount of college enrollment per"
+            "semester: "))
             if college_enrollment > 0:  # We make ensure it is a positive number
                 break
             else:
@@ -43,41 +45,43 @@ def ask_information():
 
 
 def payment_fee_calc_while_studying(credit_type, college_enrollment, semesters):
+    """Calculate the payment fee while the student is studying."""
     if credit_type == 1:
         if college_enrollment <= 0:
             raise CollegeEnrollmentError()
         if semesters <= 0 or isinstance(semesters, float):
             raise SemestersError()
-        payment_fee =  (0.3 * college_enrollment * semesters* medium_term_credit_interest_rate_30) / (1 - (1 + medium_term_credit_interest_rate_30)**(-1 * (semesters/2) * 12))
+        payment_fee = (0.3 * college_enrollment * semesters * MEDIUM_TERM_CREDIT_INTEREST_RATE_30) / (1 - (1 + MEDIUM_TERM_CREDIT_INTEREST_RATE_30)**(-1 * (semesters/2) * 12))
     elif credit_type == 2:
         if college_enrollment <= 0:
             raise CollegeEnrollmentError()
         if semesters <= 0 or isinstance(semesters, float):
             raise SemestersError()
-        payment_fee =  (0.6 * college_enrollment * semesters * medium_term_credit_interest_rate_60) / (1 - (1 + medium_term_credit_interest_rate_60)**(-1 * (semesters/2) * 12))
+        payment_fee = (0.6 * college_enrollment * semesters * MEDIUM_TERM_CREDIT_INTEREST_RATE_60) / (1 - (1 + MEDIUM_TERM_CREDIT_INTEREST_RATE_60)**(-1 * (semesters/2) * 12))
     elif credit_type == 3:
         if college_enrollment <= 0:
             raise CollegeEnrollmentError()
         if semesters <= 0 or isinstance(semesters, float):
             raise SemestersError()
-        payment_fee = (college_enrollment * semesters * short_term_credit_interest_rate_100) / (1 - (1 + short_term_credit_interest_rate_100)**(-1 * (semesters/2) * 12))
+        payment_fee = (college_enrollment * semesters * SHORT_TERM_CREDIT_INTEREST_RATE_100) / (1 - (1 + SHORT_TERM_CREDIT_INTEREST_RATE_100)**(-1 * (semesters/2) * 12))
     else:
         raise CreditTypeError()
     return payment_fee
 
 def payment_fee_calc_after_studying(credit_type, college_enrollment, semesters):
+    """Calculate the payment fee after the student has finished studying."""
     if credit_type == 1:
         if college_enrollment <= 0:
             raise CollegeEnrollmentError()
         if semesters <= 0 or isinstance(semesters, float):
             raise SemestersError()
-        payment_fee =  (0.7 * college_enrollment * semesters* medium_term_credit_interest_rate_30) / (1 - (1 + medium_term_credit_interest_rate_30)**(-1.5 * (semesters/2) * 12))
+        payment_fee = (0.7 * college_enrollment * semesters * MEDIUM_TERM_CREDIT_INTEREST_RATE_30) / (1 - (1 + MEDIUM_TERM_CREDIT_INTEREST_RATE_30)**(-1.5 * (semesters/2) * 12))
     elif credit_type == 2:
         if college_enrollment <= 0:
             raise CollegeEnrollmentError()
         if semesters <= 0 or isinstance(semesters, float):
             raise SemestersError()
-        payment_fee =  (0.4 * college_enrollment * semesters * medium_term_credit_interest_rate_60) / (1 - (1 + medium_term_credit_interest_rate_60)**(-1.5 * (semesters/2) * 12))
+        payment_fee = (0.4 * college_enrollment * semesters * MEDIUM_TERM_CREDIT_INTEREST_RATE_60) / (1 - (1 + MEDIUM_TERM_CREDIT_INTEREST_RATE_60)**(-1.5 * (semesters/2) * 12))
     elif credit_type == 3:
         return None
     else:
