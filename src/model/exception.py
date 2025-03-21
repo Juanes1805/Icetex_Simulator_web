9ffc8704836944cc7ce55b77c7c0c57ea2e3e426
Samuel GG <samuelgilest@gmail.com>
2025-03-21 02:14:41 -0500

    code cleaned with pylint

--- src/model/exception.py
+++ src/model/exception.py
@@ -1,16 +1,20 @@
+"""This module contains the custom exceptions for the model module."""
 class CollegeEnrollmentError(Exception):
+    """Exception raised for errors in the college enrollment amount."""
     def __init__(self, message="Invalid college enrollment"):
         self.message = message
 
         super().__init__(self.message)
 
 class SemestersError(Exception):
+    """Exception raised for errors in the number of semesters."""
     def __init__(self, message="Invalid number of semesters"):
         self.message = message
 
         super().__init__(self.message)
 
 class CreditTypeError(Exception):
+    """Exception raised for errors in the credit type."""
     def __init__(self, message="Invalid credit type"):
         self.message = message
 
