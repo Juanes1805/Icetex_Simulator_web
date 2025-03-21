97bfbdc5778a146e768f45715f9ce5896b1ecfbd
Juan Sebastian Pinilla Giraldo <pinillagiraldoj@gmail.com>
2025-03-21 01:09:51 -0500

    logic coments in spanish translated to english

--- src/model/logic.py
+++ src/model/logic.py
@@ -5,24 +5,24 @@ sys.path.append("src")
 from model.exception import CreditTypeError, CollegeEnrollmentError, SemestersError
 
 
-medium_term_credit_interest_rate_30 = 0.0115 #1,15% mes vencido
-medium_term_credit_interest_rate_60 = 0.099 #0,99% mes vencido
-short_term_credit_interest_rate_100 = 0.099 #0,99% mes vencido
+medium_term_credit_interest_rate_30 = 0.0115 #1,15% month expired
+medium_term_credit_interest_rate_60 = 0.099 #0,99% month expired
+short_term_credit_interest_rate_100 = 0.099 #0,99% month expired
 
 
 def ask_information():
     while True:
         credit_type = input("Enter the type of credit you want \n 1 for 30%\n 2 for 60%\n 3 for 100%\n")
         
-        if credit_type in {"1", "2", "3"}:
-            credit_type = int(credit_type)  # Convertimos a entero después de validar
+        if credit_type in ["1", "2", "3"]:
+            credit_type = int(credit_type)  # We convert to integer after validating the input
             break
         print("Invalid option. Please enter 1, 2, or 3.")
 
     while True:
         try:
             college_enrollment = float(input("Enter the amount of college enrollment per semester: "))
-            if college_enrollment > 0:  # Aseguramos que sea un número positivo
+            if college_enrollment > 0:  # We make ensure it is a positive number
                 break
             else:
                 print("Invalid amount. Please enter a positive number.")
@@ -32,7 +32,7 @@ def ask_information():
     while True:
         try:
             semesters = int(input("Enter the number of semesters you want to calculate for: "))
-            if semesters > 0:  # Aseguramos que el número de semestres sea positivo
+            if semesters > 0:  # We ensure that the number of semesters is positive
                 break
             else:
                 print("Invalid number of semesters. Please enter a positive integer.")
