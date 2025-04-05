ff608abbb63ca3e84aabd734627ab48f9192f7e1
Luis Guerra <guerraherrera000@gmail.com>
2025-04-05 14:35:56 -0500

    Se añadieron nuevos elementos a la interfaz y se corrigio nombre y tamaño de los titulos

--- src/view/gui/console_kivy.py
+++ src/view/gui/console_kivy.py
@@ -4,17 +4,33 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.anchorlayout import AnchorLayout
+from kivy.graphics import Color, Rectangle
 
 
 class Icetex_Calculator( App ):
     def build(self):
         main_container = BoxLayout(orientation = "vertical")
+        
+        # Agregar un color de fondo usando Canvas
+        with main_container.canvas.before:
+            Color(240, 240, 240, 1) 
+            self.rect = Rectangle(pos=main_container.pos, size=main_container.size)
+            
+        def update_react(instance, value):
+            self.rect.pos = instance.pos
+            self.rect.size = instance.size
+        
+        main_container.bind(pos=update_react, size=update_react)
 
-        credit_title = Label(text = "Escoge tu tipo de crédito")
+        title_welcome = Label(text = "Welcome to the ICETEX simulator", color=(0,0,0,1), font_size=30)
+  
+        container_tittle_welcome = AnchorLayout(anchor_x = "center", anchor_y = "center")
+        
+        container_tittle_welcome.add_widget(title_welcome)
+        
+        credit_title = Label(text = "Choose your credit type", color=(0,0,0,1), font_size=19)
 
         container_buttons = BoxLayout(orientation = "horizontal")
-
-        container_inputs = BoxLayout(orientation = "horizontal")
 
         #Buttons for the credit type
         btn_30_porcentage = Button(text = "30 %", background_color =(0, 0.7, 1, 1))
@@ -25,16 +41,23 @@ class Icetex_Calculator( App ):
         layout_input_cost = AnchorLayout(anchor_x = "center", anchor_y = "center")
         layout_input_quantity = AnchorLayout(anchor_x = "center", anchor_y = "center")
 
-        cost_semesteer_title = Label(text = "Ingresa el costo del semestre")
-        quantity_semesteers_title = Label(text = "Ingresa la cantidad de semestres")
+        cost_semesteer_title = Label(text = "Enter the cost of semester", color=(0,0,0,1), font_size=19)
+        quantity_semesteers_title = Label(text = "Enter the number of semesters", color=(0,0,0,1), font_size=19)
 
-        cost_semesteer = TextInput(hint_text = "Ej: '12000000'", halign= "center", size_hint=(0.3, 0.4))
-        quantity_semesteer = TextInput(hint_text = "Ej: 10", halign= "center", size_hint=(0.3, 0.4))
+        cost_semesteer = TextInput(hint_text = "e.g. (12000000)", halign= "center", size_hint=(0.5, 0.5), multiline=False)
+        quantity_semesteer = TextInput(hint_text = "e.g. (10)", halign= "center", size_hint=(0.5, 0.5), multiline=False)
 
         layout_input_cost.add_widget(cost_semesteer)
         layout_input_quantity.add_widget(quantity_semesteer)
-
-
+        
+        container_button_calculate = AnchorLayout(anchor_x = "center", anchor_y = "center")
+        calculate = Button(text = "Calculate", background_color =(0, 0.78, 0.25, 0.9), size_hint=(0.34, 1))
+        container_button_calculate.add_widget(calculate)
+        
+        
+        container_output = BoxLayout()
+        output = Label(text = "Total credit: ", halign = "center", color=(0,0,0,1), font_size=19)
+        container_output.add_widget(output)
 
         #Add widgets to the container buttons
         container_buttons.add_widget(btn_30_porcentage)
@@ -42,14 +65,16 @@ class Icetex_Calculator( App ):
         container_buttons.add_widget(btn_100_porcentage)
 
         #Add widges to the container inputs
-
-
+        main_container.add_widget(container_tittle_welcome)
         main_container.add_widget(credit_title)
         main_container.add_widget(container_buttons)
         main_container.add_widget(cost_semesteer_title)
         main_container.add_widget(layout_input_cost)
         main_container.add_widget(quantity_semesteers_title)
         main_container.add_widget(layout_input_quantity)
+        main_container.add_widget(container_button_calculate)
+        main_container.add_widget(container_output)
+        
         return main_container
     
         
