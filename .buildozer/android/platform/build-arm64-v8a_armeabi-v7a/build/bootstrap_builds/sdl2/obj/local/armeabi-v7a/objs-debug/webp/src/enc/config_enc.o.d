fe93f3df557cd25376fac47afea047a5bc4f3f89
Luis Guerra <110359937+sgGuerra@users.noreply.github.com>
2025-04-30 21:00:04 -0500

    Delete .buildozer directory

--- .buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/webp/src/enc/config_enc.o.d
+++
@@ -1,8 +0,0 @@
-/home/guerra/icetex_simulator_kivy/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/webp/src/enc/config_enc.o: \
-  /home/guerra/icetex_simulator_kivy/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/enc/config_enc.c \
-  /home/guerra/icetex_simulator_kivy/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/webp/encode.h \
-  /home/guerra/icetex_simulator_kivy/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/webp/./types.h
-
-/home/guerra/icetex_simulator_kivy/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/webp/encode.h:
-
-/home/guerra/icetex_simulator_kivy/.buildozer/android/platform/build-arm64-v8a_armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL2_image/external/libwebp/src/webp/./types.h:
