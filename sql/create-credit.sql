654dc8b4e0a8a06bc01dcee6c9dd2163e8c32c98
Samuel GG <samuelgilest@gmail.com>
2025-05-13 22:56:49 -0500

    credit_type column has now a maximum of 10 characters

--- sql/create-credit.sql
+++ sql/create-credit.sql
@@ -2,7 +2,7 @@ create table credits (
   credit_id varchar(50) primary key not null,
   college_enrollment decimal,
   semesters int,
-  credit_type varchar(4),
+  credit_type varchar(10),
   payment_fee_while_studying decimal,
   payment_fee_after_studying decimal
 );
