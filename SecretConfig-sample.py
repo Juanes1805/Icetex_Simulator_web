8fcfe9a7ac087d798f394512c01d0d1d245025fd
Samuel GG <samuelgilest@gmail.com>
2025-05-13 19:43:23 -0500

    added SecretConfig-sample file

---
+++ SecretConfig-sample.py
@@ -0,0 +1,7 @@
+# Write here your connection data and save as SecretConfig.py
+# Do not expose your Neon credentials to the browser
+
+PGHOST='Database host here'
+PGDATABASE='Database name here'
+PGUSER='Database user here'
+PGPASSWORD='Password here'
