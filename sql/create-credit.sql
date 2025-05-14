e29b6c9e46a95f74771e2589aca1fab42580677e
Samuel GG <samuelgilest@gmail.com>
2025-05-13 19:23:01 -0500

    created sql file to create credits table

---
+++ sql/create-credit.sql
@@ -0,0 +1,8 @@
+create table credits (
+  credit_id varchar(50) primary key not null,
+  college_enrollment decimal,
+  semesters int,
+  credit_type varchar(4),
+  payment_fee_while_studying decimal,
+  payment_fee_after_studying decimal
+);
